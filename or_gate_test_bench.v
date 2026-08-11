8f027fd6580f60020bfc5c8e4d69176a28212e13
Sinchana H M <sinchu0211@gmail.com>
2026-08-11 21:52:55 +0530

    Create or_gate_test_bench.v

---
+++ or_gate_test_bench.v
@@ -0,0 +1,27 @@
+module test;
+
+reg a, b;
+wire y;
+
+orgate uut(a, b, y);
+
+initial
+begin
+    
+
+    a=0; b=0;
+    #10;
+
+    a=0; b=1;
+    #10;
+
+    a=1; b=0;
+    #10;
+
+    a=1; b=1;
+    #10;
+
+    $finish;
+end
+
+endmodule
